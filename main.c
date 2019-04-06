aed92fe745bd2b3017f3887e0825fca3236619ee
demiurg337 <dmitro.gedz@gmail.com>
2019-04-06 21:41:48 +0300

    First method for creation of socket

--- main.c
+++ main.c
@@ -1,4 +1,7 @@
-struct Chat {
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+struct messenger {
 //net
 
 //dht
@@ -7,8 +10,55 @@ struct Chat {
 //log
 //crypto
 //options
+};
+
+
+struct Ip {
+};
+
+
+/*
+
+tox_iterate
+
+I -> messager
+-> friends (list of users) 
+-> rooms (list of users) 
+
+user
+-> name
+
+
+*/
+
+////////////////////////////////////
+////////////////////////////////////
+////////////////////////////////////
+////////////////////////////////////
+//Network
+struct Network {
+};
+
+typedef struct Socket {
+    int f_descriptor;
+} Socket;
+
+void new_network()
+{
+    //if it isn't ip4 and ip6
+    //checking
 }
 
+Socket new_socket() {
+    //see UDP hole punching
+    Socket sock = {(int) socket(AF_UNSPEC, SOCK_DGRAM, IPPROTO_UDP)};
+    return sock; 
+}
+
+///////////////////////////////////////////////////
+///////////////////////////////////////////////////
+///////////////////////////////////////////////////
 
 int main() {
 }
+
