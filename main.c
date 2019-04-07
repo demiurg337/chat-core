7985752af0614c95cb1248bce36c5231a76eed85
demiurg337 <dmitro.gedz@gmail.com>
2019-04-07 22:06:25 +0300

    First code for logging

--- main.c
+++ main.c
@@ -36,6 +36,27 @@ user
 ////////////////////////////////////
 ////////////////////////////////////
 ////////////////////////////////////
+//logger
+#include <stdio.h>
+
+typedef enum Log_lvl {
+    IS_LOG_TRACE,
+    IS_LOG_DEBUG,
+    IS_LOG_INFO,
+    IS_LOG_WARN,
+    IS_LOG_ERR
+} Log_lvl;
+
+void log(const char* msg, const char* file, const char* func, const char* line) {
+    printf("TTTTTTTTTTTT");
+}
+
+#define LOG(msg, Log_lvl);
+
+////////////////////////////////////
+////////////////////////////////////
+////////////////////////////////////
+////////////////////////////////////
 //Network
 
 typedef struct Socket {
@@ -44,6 +65,7 @@ typedef struct Socket {
 
 
 Socket new_socket() {
+    //see UDP hole punching
     int sock_d = (int) socket(AF_UNSPEC, SOCK_DGRAM, IPPROTO_UDP);
     int max_buf = 1024 * 1024 * 2;//2 Megabites
     //set max buffer size
@@ -55,7 +77,6 @@ Socket new_socket() {
     //enable broadcast (shorokoveshatelniu) address (only for udp)
     setsockopt(sock_d, SOL_SOCKET, SO_BROADCAST, &is_enabled, sizeof(is_enabled)); 
 
-    //see UDP hole punching
     Socket sock = {sock_d};
     return sock; 
 }
@@ -72,7 +93,11 @@ Network* new_network()
     //Network* net = (Network*) calloc(1, sizeof(Network));
     Network* net = (Network*) calloc(1, sizeof(Network));
     net->socket = new_socket();
-
+    /*
+    !!!!!!!!!!!!!!!!!!!! 
+    FREEEE NET !!!!!!!!!!!!!!!!!!!!
+    !!!!!!!!!!!
+    */
     
 
     return net;
