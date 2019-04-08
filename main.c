aad9edbc78670d05ec0af04438aa8ae7f8f108ed
demiurg337 <dmitro.gedz@gmail.com>
2019-04-08 09:13:59 +0300

    Main functional for logging

--- main.c
+++ main.c
@@ -47,16 +47,31 @@ typedef enum Log_lvl {
     IS_LOG_ERR
 } Log_lvl;
 
-void log(const char* file, const char* func, const char* line, const char* msg) {
-    printf("TTTTTTTTTTTT");
+void log(const char* file, const char* line, const char* func, const char* msg) {
+    printf("%s:%s;%s: %s", file, line, func, msg);
 }
 
 #define MIN_LOG_LVL IS_LOG_TRACE
 #define LOG(level, msg) \
     do { \
         if (level >= MIN_LOG_LVL) {\
+            log(__FILE__, /*__LINE__*/"zzz", __func__, /*msg*/"zzz");\
         }\
     } while(0)
+
+#define LOG_TRACE(msg) LOG(IS_LOG_TRACE, msg)
+#define LOG_DEBUG(msg) LOG(IS_LOG_DEBUG, msg)
+#define LOG_INFO(msg)  LOG(IS_LOG_INFO, msg)
+#define LOG_WARN(msg)  LOG(IS_LOG_WARN, msg)
+#define LOG_ERR(msg)   LOG(IS_LOG_ERR, msg)
+
+#define LOG_FATAL(msg) \
+    do {\
+        LOG_ERR(msg);\
+        abort();\
+    } while(0)
+
+
 
 ////////////////////////////////////
 ////////////////////////////////////
@@ -113,6 +128,7 @@ Network* new_network()
 
 int main() {
     new_network();
+    LOG_TRACE("UEUEUEUE");
 }
 
 
