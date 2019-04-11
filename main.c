15829d6f05a6025411c7e68eb1148cde998284c5
demiurg337 <dmitro.gedz@gmail.com>
2019-04-11 14:04:14 +0300

    Additional option for socket

--- main.c
+++ main.c
@@ -64,10 +64,10 @@ int new_socket() {
     setsockopt(sock_d, SOL_SOCKET, SO_BROADCAST, &is_enabled, sizeof(is_enabled)); 
 
     //make socket not blocking
-    if (fcntl(sock_d, F_SETFL, O_NONBLOCK, 1) == 0) {
-        return 0;
+    if (fcntl(sock_d, F_SETFL, O_NONBLOCK, 1) < 0) {
+        return -1;
     }
-
+    
     return sock_d; 
 }
 
@@ -83,6 +83,9 @@ Network* new_network()
     //Network* net = (Network*) calloc(1, sizeof(Network));
     Network* net = (Network*) calloc(1, sizeof(Network));
     net->socket_d = new_socket();
+    if (net->socket_d < 0) {
+        LOG_FATAL("Cant create socket !");
+    }
     /*
     !!!!!!!!!!!!!!!!!!!! 
     FREEEE NET !!!!!!!!!!!!!!!!!!!!
@@ -92,6 +95,33 @@ Network* new_network()
     memset(&addr, 0, sizeof(sockaddr_storage));
     
     //bind socket to some address and port 
+    //will use ip6
+    struct sockaddr_in6* addr6 = (struct sockaddr_in6*) &addr;
+    addr6->sin6_family = AF_INET6;
+    // can be without htonl and htons, 
+    // but they were added for understanding process
+    // all need to convert to "network" format (by for "0" it can be without it)
+    addr6->sin6_port = htons(0);
+    // for ipv4 can be
+    // sin6.sin6_addr = INADDR_ANY_INIT 
+    //servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr6->sin6_addr = in6addr_any; 
+    //???
+    addr6->sin6_flowinfo = 0;
+    addr6->sin6_scope_id = 0;
+
+    int 
+        v6only_need_val = 0,
+        v6only_val = 0;
+        socklen_t v6only_optsize = sizeof (v6only_val);
+    if (
+        getsockopt(net->socket_d, IPPROTO_IPV6, IPV6_V6ONLY, &v6only_val, &v6only_optsize) == 0
+        &&
+        v6only_val != v6only_need_val
+    ) {
+        v6only_val = v6only_need_val;
+        setsockopt(net->socket_d, IPPROTO_IPV6, IPV6_V6ONLY, &v6only_need_val, sizeof(v6only_need_val));
+    }
 
 
 
