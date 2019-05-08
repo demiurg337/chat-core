5e5fe73cd833792ceb62dafa2ff20df02a67f853
demiurg337 <dmitro.gedz@gmail.com>
2019-05-08 08:01:43 +0300

    Fix for command for compilation

--- com.sh
+++ com.sh
@@ -1,2 +1,4 @@
 #g++ -I ./src -Wall -pedantic -std=iso9899:1999  src/main.c src/log.c -o res
-g++ -I ../src -Wall -pedantic -std=iso9899:1999  ../src/*.c -o res
+# gcc - for compiling c and c++ code
+# g++ - for compiling only c++
+gcc -I ../src -Wall -pedantic -std=iso9899:1999  ../src/*.c -o res
