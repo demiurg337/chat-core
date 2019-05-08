ab5a3889dfa5a0c72cb34e83acbcacc4c5385370
demiurg337 <dmitro.gedz@gmail.com>
2019-05-08 16:43:27 +0300

    Command for compilation has been deleted

--- com.sh
+++
@@ -1,4 +0,0 @@
-#g++ -I ./src -Wall -pedantic -std=iso9899:1999  src/main.c src/log.c -o res
-# gcc - for compiling c and c++ code
-# g++ - for compiling only c++
-gcc -I ../src -Wall -pedantic -std=iso9899:1999  ../src/*.c -o res
