ec29a1636f1ceb8a06c638b3833aed3d87e9100c
demiurg337 <dmitro.gedz@gmail.com>
2019-05-09 16:19:07 +0300

    Guardian has been added to utils.h

--- src/utils.h
+++ src/utils.h
@@ -1,2 +1,7 @@
+#ifndef C_CHAT_CORE_UTILS
+#define C_CHAT_CORE_UTILS
+
 void* safe_calloc(size_t count_els, size_t el_size);
 void* safe_realloc(void* ptr, size_t new_size);
+
+#endif //C_CHAT_CORE_UTILS
